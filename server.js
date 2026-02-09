465a8ae214decf60b5f29dfff0432711fa844f60
DhirajPrajapati26 <26dhirajdp@gmail.com>
2026-02-09 12:46:39 +0530

    update code

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://your-frontend.vercel.app"],
+    origin: ["http://localhost:5173", "https://notehub-phi.vercel.app"],
     credentials: true,
   }),
 );
